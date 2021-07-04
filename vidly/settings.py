9e96477be6ac1f6a637bec0df829c26884294215
Fen <tozhengfen@gmail.com>
2021-07-04 14:50:56 +0200

    add requirement

--- vidly/settings.py
+++ vidly/settings.py
@@ -55,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'vidly.urls'
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
